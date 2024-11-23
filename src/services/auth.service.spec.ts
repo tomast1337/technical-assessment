98e0de8f83fa56ae05f208ef35acc41c0888409e
tomast1337 <vycasnicolas@gmail.com>
2024-11-23 12:08:53 -0300

    Refactor auth service tests for improved structure and async handling

--- src/services/auth.service.spec.ts
+++ src/services/auth.service.spec.ts
@@ -5,15 +5,18 @@ import { expect } from 'chai';
 import * as mongoose from 'mongoose';
 import * as sinon from 'sinon';
 
+import '../database';
 import GeoLib from '../lib';
+import '../server';
 import { loginUser, me, registerUser, updatePassword } from './auth.service';
 describe('Auth Service', () => {
+  let session;
   let sandbox: sinon.SinonSandbox;
   const geoLibStub: Partial<typeof GeoLib> = {};
 
-  beforeEach(() => {
+  before(async () => {
     sandbox = sinon.createSandbox();
-
+    session = await mongoose.startSession();
     // Mock GeoLib methods
     geoLibStub.getAddressFromCoordinates = sinon
       .stub(GeoLib, 'getAddressFromCoordinates')
@@ -27,20 +30,26 @@ describe('Auth Service', () => {
       });
   });
 
-  afterEach(() => {
-    sandbox.restore();
+  after(async () => {
+    sinon.restore();
+    await session.endSession();
+  });
+
+  beforeEach(async () => {
+    await session.startTransaction();
+  });
+
+  afterEach(async () => {
+    await UserModel.deleteMany({});
+    await session.abortTransaction();
   });
 
   describe('registerUser', () => {
     it('should register a new user', async () => {
-      const name = faker.person.firstName() + ' ' + faker.person.lastName();
+      const name = faker.person.firstName();
       const email = faker.internet.email();
       const password = faker.internet.password();
-      const address = faker.location.streetAddress();
-
-      (UserModel.findOne as sinon.SinonStub).resolves(null);
-      const userInstance = new UserModel({ name, email, password, address });
-      (userInstance.save as sinon.SinonStub).resolves(userInstance);
+      const address = faker.location.streetAddress({ useFullAddress: true });
 
       const result = await registerUser({ name, email, password, address });
 
@@ -48,31 +57,18 @@ describe('Auth Service', () => {
         'message',
         'User registered successfully',
       );
-
-      sinon.assert.calledOnce(userInstance.save as sinon.SinonStub);
-
-      sinon.assert.calledOnce(
-        GeoLib.getCoordinatesFromAddress as sinon.SinonStub,
-      );
     });
 
     it('should throw an error if the user already exists', async () => {
+      const name = faker.person.firstName();
       const email = faker.internet.email();
+      const password = faker.internet.password();
+      const address = faker.location.streetAddress({ useFullAddress: true });
 
-      sandbox.stub(UserModel, 'findOne').resolves({
-        name: faker.person.firstName(),
-        email,
-        password: faker.internet.password(),
-        address: faker.location.streetAddress(),
-      });
+      await registerUser({ name, email, password, address });
 
       try {
-        await registerUser({
-          name: faker.person.firstName(),
-          email,
-          password: faker.internet.password(),
-          address: faker.location.streetAddress(),
-        });
+        await registerUser({ name, email, password, address });
       } catch (error) {
         expect(error).to.have.property('message', 'User already exists');
       }
@@ -80,99 +76,57 @@ describe('Auth Service', () => {
   });
 
   describe('loginUser', () => {
-    it('should login a user and return a token', async () => {
+    it('should login a user and return a token and refresh token', async () => {
+      const name = faker.person.firstName();
       const email = faker.internet.email();
       const password = faker.internet.password();
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const address = faker.location.streetAddress({ useFullAddress: true });
 
-      sandbox.stub(UserModel, 'findOne').resolves({
-        _id: new mongoose.Types.ObjectId(),
-        name: faker.person.firstName(),
-        email,
-        password: hashedPassword,
-      });
+      await registerUser({ name, email, password, address });
 
       const result = await loginUser({ email, password });
 
       expect(result).to.have.property('token');
+      expect(result).to.have.property('refreshToken');
     });
 
     it('should throw an error if the credentials are invalid', async () => {
-      sandbox.stub(UserModel, 'findOne').resolves(null);
+      const email = faker.internet.email();
+      const password = faker.internet.password();
 
       try {
-        await loginUser({
-          email: faker.internet.email(),
-          password: faker.internet.password(),
-        });
+        await loginUser({ email, password });
       } catch (error) {
         expect(error).to.have.property('message', 'Invalid credentials');
       }
     });
   });
 
-  describe('me', () => {
-    it('should return the user details', async () => {
-      const email = faker.internet.email();
-      const password = faker.internet.password();
-
-      const user = {
-        _id: new mongoose.Types.ObjectId(),
-        name: faker.person.firstName() + ' ' + faker.person.lastName(),
-        email,
-        password,
-      };
-
-      sandbox.stub(UserModel, 'findOne').resolves(user);
-
-      const result = await me(user._id.toString());
-
-      expect(result).to.have.property('email', email);
-    });
-
-    it('should throw an error if the user is not found', async () => {
-      sandbox.stub(UserModel, 'findOne').resolves(null);
-
-      try {
-        await me(new mongoose.Types.ObjectId().toString());
-      } catch (error) {
-        expect(error).to.have.property('message', 'User not found');
-      }
-    });
-  });
-
   describe('updatePassword', () => {
     it('should update the user password', async () => {
+      const name = faker.person.firstName();
       const email = faker.internet.email();
       const password = faker.internet.password();
+      const address = faker.location.streetAddress({ useFullAddress: true });
 
-      const user = {
-        _id: new mongoose.Types.ObjectId(),
-        name: faker.person.firstName(),
-        email,
-        password: bcrypt.hashSync(password, 10),
-        address: faker.location.streetAddress(),
-      };
+      await registerUser({ name, email, password, address });
 
-      sandbox.stub(UserModel, 'findOne').resolves(user);
+      const user = await UserModel.findOne({ email });
 
       const newPassword = faker.internet.password();
-      const result = await updatePassword(user._id.toString(), newPassword);
+      await updatePassword(user._id, newPassword);
 
-      expect(result).to.have.property(
-        'message',
-        'Password updated successfully',
-      );
+      const updatedUser = await UserModel.findById(user._id);
+      const isMatch = await bcrypt.compare(newPassword, updatedUser.password);
+
+      expect(isMatch).to.be.true;
     });
 
     it('should throw an error if the user is not found', async () => {
-      sandbox.stub(UserModel, 'findOne').resolves(null);
+      const newPassword = faker.internet.password();
 
       try {
-        await updatePassword(
-          new mongoose.Types.ObjectId().toString(),
-          faker.internet.password(),
-        );
+        await updatePassword(faker.string.alphanumeric(10), newPassword);
       } catch (error) {
         expect(error).to.have.property('message', 'User not found');
       }
