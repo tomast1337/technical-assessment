a52c6beba403c510844d6fc0b504b30a2454684a
tomast1337 <vycasnicolas@gmail.com>
2024-11-22 21:05:25 -0300

    Implement environment variable validation using class-validator and dotenv

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,58 @@
-export const env = {
-  MONGO_URI:
-    "mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin",
-  JWT_SECRET: "b2ZmaWNlLWJhY2tlbmQtc2VjcmV0LWtleQ==",
-  JWT_EXPIRES_IN: "1d",
-  JWT_REFRESH_SECRET: "b2ZmaWNlLWJhY2tlbmQtc2VjcmV0LXJlZnJlc2g=",
-  JWT_REFRESH_EXPIRES_IN: "7d",
-  PORT: 3000,
-  NODE_ENV: "development",
-};
+import { plainToClass } from 'class-transformer';
+import { IsString, IsNotEmpty, IsNumber, validateSync } from 'class-validator';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+class Environment {
+  @IsString()
+  @IsNotEmpty()
+  MONGO_URI: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_SECRET: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_EXPIRES_IN: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_REFRESH_SECRET: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_REFRESH_EXPIRES_IN: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  NODE_ENV: string;
+
+  @IsString()
+  @IsNotEmpty()
+  GOOGLE_MAPS_API_KEY: string;
+}
+
+// Transform and validate environment variables
+const env = plainToClass(Environment, {
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN,
+  PORT: parseInt(process.env.PORT, 10),
+  NODE_ENV: process.env.NODE_ENV,
+  GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+});
+
+const errors = validateSync(env, { skipMissingProperties: false });
+
+if (errors.length > 0) {
+  throw new Error(errors.toString());
+}
+
+export { env };
