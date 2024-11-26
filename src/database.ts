7d2f30cdeeb9d76ebf07fcda5f904645dac9c615
tomast1337 <vycasnicolas@gmail.com>
2024-11-25 21:09:52 -0300

    Add logging for database connection and update region model coordinates type

--- src/database.ts
+++ src/database.ts
@@ -7,6 +7,7 @@ const loggerLocal = logger.child({ label: 'database' });
 
 const init = async function () {
   try {
+    loggerLocal.info(`Connecting to database: ${env.MONGO_URI}`);
     await mongoose.connect(env.MONGO_URI);
     mongoose.set('debug', env.NODE_ENV === 'development');
     loggerLocal.info('Database connected');
