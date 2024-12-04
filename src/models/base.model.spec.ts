f65f87fc7a0392a5435262373c6b64df3ba8697d
tomast1337 <vycasnicolas@gmail.com>
2024-12-04 16:15:21 -0300

    Refactor model tests to utilize mongoose sessions for transaction management and improve database interaction

--- src/models/base.model.spec.ts
+++ src/models/base.model.spec.ts
@@ -3,9 +3,7 @@ import { faker } from '@faker-js/faker';
 import { Prop, getModelForClass } from '@typegoose/typegoose';
 import { expect } from 'chai';
 import * as mongoose from 'mongoose';
-import { restore, stub } from 'sinon';
-import '../database';
-import '../server';
+import db from '../database';
 
 import { Base } from './base.model';
 
@@ -19,57 +17,46 @@ const TestModelClass = getModelForClass(TestModel);
 
 describe('Base Model', () => {
   let session: mongoose.ClientSession;
-  let createStub: sinon.SinonStub;
-  let findOneStub: sinon.SinonStub;
 
-  before(() => {
-    session = {
-      startTransaction: stub(),
-      abortTransaction: stub(),
-      endSession: stub(),
-    } as unknown as mongoose.ClientSession;
-
-    createStub = stub(TestModelClass, 'create');
-    findOneStub = stub(TestModelClass, 'findOne');
+  before(async () => {
+    await db;
+    session = await mongoose.startSession();
   });
 
   after(() => {
-    restore();
+    session.endSession();
   });
 
   beforeEach(() => {
     session.startTransaction();
   });
 
-  afterEach(() => {
-    createStub.reset();
-    findOneStub.reset();
-    session.abortTransaction();
+  afterEach(async () => {
+    try {
+      await session.abortTransaction();
+    } finally {
+      session.endSession();
+      session = await mongoose.startSession();
+    }
   });
 
   it('should create a new document', async () => {
     const name = faker.person.firstName();
-    const testUser = [{ name }];
-    createStub.resolves(testUser);
 
-    const result = await TestModelClass.create([{ name }]);
+    const result = await TestModelClass.create([{ name }], { session });
     expect(result[0].name).to.equal(name);
-    expect(createStub.calledOnce).to.be.true;
   });
 
   it('should find a document', async () => {
     const name = faker.person.firstName();
-    const testUser = { name };
-    findOneStub.resolves(testUser);
 
-    await TestModelClass.create([{ name }]);
-    const result = await TestModelClass.findOne({ name });
+    await TestModelClass.create([{ name }], { session });
+    const result = await TestModelClass.findOne({ name }).session(session);
 
     if (!result) {
       throw new Error('User not found');
     }
 
     expect(result.name).to.equal(name);
-    expect(findOneStub.calledOnce).to.be.true;
   });
 });
