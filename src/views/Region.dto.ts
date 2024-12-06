6381c44f2b9ed05fcde5669ccd874a4bfc22804c
tomast1337 <vycasnicolas@gmail.com>
2024-12-06 12:31:47 -0300

    Refactor PageDto constructor and simplify RegionDto structure by removing unused validation

--- src/views/Region.dto.ts
+++ src/views/Region.dto.ts
@@ -3,7 +3,6 @@ import {
   ArrayMinSize,
   IsArray,
   IsNotEmpty,
-  IsObject,
   IsString,
   Validate,
   ValidateNested,
@@ -13,43 +12,16 @@ import {
 } from 'class-validator';
 import 'reflect-metadata';
 
-@ValidatorConstraint({ name: 'isFirstAndLastSame', async: false })
-class IsFirstAndLastSameConstraint implements ValidatorConstraintInterface {
-  validate(coordinates: [number, number][], _args: ValidationArguments) {
-    if (!Array.isArray(coordinates) || coordinates.length < 4) {
-      return false;
-    }
+export class RegionDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    const first = coordinates[0];
-    const last = coordinates[coordinates.length - 1];
-    return first[0] === last[0] && first[1] === last[1];
-  }
-
-  defaultMessage(_args: ValidationArguments) {
-    return 'The last item in the coordinates array should be the same as the first';
-  }
-}
-
-class GeometryDto {
   @IsString()
   @IsNotEmpty()
   type: string;
 
   @IsArray()
   @ArrayMinSize(1)
-  @ValidateNested({ each: true })
-  @Type(() => Array)
-  @Validate(IsFirstAndLastSameConstraint)
   coordinates: [number, number][][];
-}
-
-export class RegionDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
-  @IsObject()
-  @ValidateNested()
-  @Type(() => GeometryDto)
-  geometry: GeometryDto;
 }
