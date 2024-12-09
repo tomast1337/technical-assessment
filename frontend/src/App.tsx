a25b972a39f05d97cc085fa5cd061a98a83f082d
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 19:45:12 -0300

    Wrap App component with I18nProvider for internationalization support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { AppRoutes } from "@routes/index";
 import { HashRouter } from "react-router-dom";
+import { I18nProvider } from "./hooks/useI18n";
+
 export const App = () => {
   return (
-    <HashRouter>
-      <AppRoutes />
-    </HashRouter>
+    <I18nProvider>
+      <HashRouter>
+        <AppRoutes />
+      </HashRouter>
+    </I18nProvider>
   );
 };
