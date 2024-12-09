2a9999c10509e7f49218612af566b1ef5319f44b
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 18:50:12 -0300

    Add authentication pages: LoginPage, RegisterPage, and UpdatePasswordPage

---
+++ frontend/src/pages/Auth/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@app/components/Auth/LoginFrom";
+import { AppLayout } from "@app/components/Layout/AppLayout";
+
+export const LoginPage = () => {
+  return (
+    <AppLayout>
+      <LoginForm />
+    </AppLayout>
+  );
+};
