bb9eb6c8a2102c053008de8e91cfb3991acf9bd2
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 22:11:30 -0300

    Remove unused frontend files and components to clean up the project structure

--- frontend/app/desktop/src/view/personnel/PersonnelViewController.js
+++
@@ -1,9 +0,0 @@
-Ext.define('frontend.view.personnel.PersonnelViewController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.personnelviewcontroller',
-
-    onEditCancelled: function (editor, value, startValue, eOpts) {
-        var user = Ext._find(value.record.store.config.data.items, { name: value.record.data.name });
-        Ext.Msg.confirm('Confirm', value.record.data.name + ': ' + user.phone + ' is phone number', 'onConfirm', this);
-    }
-});
