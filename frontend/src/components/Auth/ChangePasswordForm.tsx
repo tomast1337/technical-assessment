79bae8230536fd4282199e6c7e9db58a8f3cc54f
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 22:11:30 -0300

    Refactor ChangePasswordForm to support internationalization with react-intl

--- frontend/src/components/Auth/ChangePasswordForm.tsx
+++ frontend/src/components/Auth/ChangePasswordForm.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { Form, Input, Button, notification } from "antd";
+import { UpdatePassword } from "@app/services/Auth/types";
+import { FormattedMessage, useIntl } from "react-intl";
 
 export const ChangePasswordForm: React.FC = () => {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm<UpdatePassword>();
+  const intl = useIntl();
 
-  const onFinish = (values: any) => {
+  const onFinish = (values: UpdatePassword) => {
     console.log("Password change submitted:", values);
     notification.success({
-      message: "Password Changed",
-      description: "Your password has been successfully updated.",
+      message: intl.formatMessage({ id: "changePassword.success" }),
+      description: intl.formatMessage({
+        id: "changePassword.success.description",
+      }),
     });
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const validatePassword = ({ getFieldValue }: any) => ({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     validator(_: any, value: string) {
       if (!value || getFieldValue("newPassword") === value) {
         return Promise.resolve();
       }
-      return Promise.reject(new Error("Passwords do not match!"));
+      return Promise.reject(
+        new Error(
+          intl.formatMessage({ id: "changePassword.confirmNewPassword.match" })
+        )
+      );
     },
   });
 
@@ -32,34 +43,55 @@ export const ChangePasswordForm: React.FC = () => {
       {/* New Password */}
       <Form.Item
         name="newPassword"
-        label="New Password"
+        label={<FormattedMessage id="changePassword.newPassword" />}
         rules={[
-          { required: true, message: "Please enter a new password" },
-          { min: 6, message: "Password must be at least 6 characters" },
+          {
+            required: true,
+            message: intl.formatMessage({
+              id: "changePassword.newPassword.required",
+            }),
+          },
+          {
+            min: 6,
+            message: intl.formatMessage({
+              id: "changePassword.newPassword.min",
+            }),
+          },
         ]}
         hasFeedback
       >
-        <Input.Password placeholder="New Password" />
+        <Input.Password
+          placeholder={intl.formatMessage({ id: "changePassword.newPassword" })}
+        />
       </Form.Item>
 
       {/* Confirm New Password */}
       <Form.Item
         name="confirmNewPassword"
-        label="Confirm New Password"
+        label={<FormattedMessage id="changePassword.confirmNewPassword" />}
         dependencies={["newPassword"]}
         rules={[
-          { required: true, message: "Please confirm your new password" },
+          {
+            required: true,
+            message: intl.formatMessage({
+              id: "changePassword.confirmNewPassword.required",
+            }),
+          },
           validatePassword,
         ]}
         hasFeedback
       >
-        <Input.Password placeholder="Confirm New Password" />
+        <Input.Password
+          placeholder={intl.formatMessage({
+            id: "changePassword.confirmNewPassword",
+          })}
+        />
       </Form.Item>
 
       {/* Submit Button */}
       <Form.Item>
         <Button type="primary" htmlType="submit" block>
-          Change Password
+          <FormattedMessage id="changePassword.submit" />
         </Button>
       </Form.Item>
     </Form>
