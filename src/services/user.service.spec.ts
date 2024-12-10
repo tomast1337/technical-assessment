77ca33e3afcaf3afd786228536b54ac25364f77c
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 22:11:30 -0300

    Refactor user service tests to auth service tests and implement user registration, login, and password update functionalities

--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -1,151 +1,237 @@
+import bcrypt from 'bcryptjs';
 import { expect } from 'chai';
+import jwt from 'jsonwebtoken';
 import sinon from 'sinon';
 
+import lib from '@app/lib';
 import { UserModel } from '@models/index';
-import { User } from '@models/user.model';
 
-import UserService from './user.service';
-import { PagingDto } from '../views/Paging.dto';
+import AuthService from './auth.service';
 
-describe('UserService', () => {
-  let findByIdStub: sinon.SinonStub;
-  let findByIdAndUpdateStub: sinon.SinonStub;
-  let findByIdAndDeleteStub: sinon.SinonStub;
-  let findStub: sinon.SinonStub;
+describe('AuthService', () => {
+  let findOneStub: sinon.SinonStub;
+  let createStub: sinon.SinonStub;
+  let hashStub: sinon.SinonStub;
+  let compareStub: sinon.SinonStub;
+  let signStub: sinon.SinonStub;
+  let verifyStub: sinon.SinonStub;
+  let updateOneStub: sinon.SinonStub;
 
   beforeEach(() => {
-    findByIdStub = sinon.stub(UserModel, 'findById').returns({
-      select: sinon.stub().resolves({ _id: 'userId', name: 'John Doe' }),
-    } as any);
-
-    findByIdAndUpdateStub = sinon.stub(UserModel, 'findByIdAndUpdate').returns({
-      select: sinon.stub().resolves({ _id: 'userId', name: 'Updated Name' }),
-    } as any);
-
-    findByIdAndDeleteStub = sinon
-      .stub(UserModel, 'findByIdAndDelete')
-      .resolves({
-        _id: 'userId',
-        name: 'John Doe',
-      });
-
-    findStub = sinon.stub(UserModel, 'find').returns({
-      sort: sinon.stub().returnsThis(),
-      skip: sinon.stub().returnsThis(),
-      limit: sinon.stub().returnsThis(),
-      select: sinon.stub().resolves([{ _id: 'userId', name: 'John Doe' }]),
-    } as any);
+    findOneStub = sinon.stub(UserModel, 'findOne');
+    createStub = sinon.stub(UserModel, 'create');
+    hashStub = sinon.stub(bcrypt, 'hash');
+    compareStub = sinon.stub(bcrypt, 'compare');
+    signStub = sinon.stub(jwt, 'sign');
+    verifyStub = sinon.stub(jwt, 'verify');
+    updateOneStub = sinon.stub(UserModel, 'updateOne');
   });
 
   afterEach(() => {
     sinon.restore();
   });
 
-  describe('getUserById', () => {
-    it('should return a user if found', async () => {
-      const user = { _id: 'userId', name: 'John Doe' };
+  describe('registerUser', () => {
+    it('should register a new user', async () => {
+      findOneStub.resolves(null);
+      hashStub.resolves('hashedPassword');
+      createStub.resolves();
 
-      findByIdStub.returns({
-        select: sinon.stub().resolves(user),
-      } as any);
+      const result = await AuthService.registerUser({
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: 'password123',
+        address: '123 Main St',
+      });
 
-      const result = await UserService.getUserById(user as User, 'userId');
-
-      expect(result).to.deep.equal(user);
-      expect(findByIdStub.calledOnce).to.be.true;
+      expect(result).to.deep.equal({ message: 'User registered successfully' });
+      expect(findOneStub.calledOnce).to.be.true;
+      expect(hashStub.calledOnce).to.be.true;
+      expect(createStub.calledOnce).to.be.true;
     });
 
-    it('should throw an error if the user is not found', async () => {
-      findByIdStub.returns({
-        select: sinon.stub().resolves(null),
-      } as any);
+    it('should throw an error if the user already exists', async () => {
+      findOneStub.resolves({});
 
       try {
-        await UserService.getUserById({} as User, 'userId');
-      } catch (error) {
-        expect((error as any).message).to.equal('User not found');
-      }
-
-      expect(findByIdStub.calledOnce).to.be.true;
-    });
-  });
-
-  describe('updateUser', () => {
-    it('should update a user if found', async () => {
-      const updateData = { name: 'Updated Name' };
-      const updatedUser = { _id: 'userId', ...updateData };
-
-      findByIdAndUpdateStub.returns({
-        select: sinon.stub().resolves(updatedUser),
-      } as any);
-
-      const result = await UserService.updateUser(
-        {} as User,
-        'userId',
-        updateData,
-      );
-
-      expect(result).to.deep.equal(updatedUser);
-      expect(findByIdAndUpdateStub.calledOnce).to.be.true;
-    });
-
-    it('should throw an error if the user is not found', async () => {
-      findByIdAndUpdateStub.returns({
-        select: sinon.stub().resolves(null),
-      } as any);
-
-      try {
-        await UserService.updateUser({} as User, 'userId', {
-          name: 'Updated Name',
+        await AuthService.registerUser({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'password123',
+          address: '123 Main St',
         });
       } catch (error) {
-        expect((error as any).message).to.equal('User not found');
+        expect((error as any).message).to.equal('User already exists');
       }
 
-      expect(findByIdAndUpdateStub.calledOnce).to.be.true;
+      expect(findOneStub.calledOnce).to.be.true;
+    });
+
+    it('should throw an error if both address and coordinates are provided', async () => {
+      findOneStub.resolves(null);
+
+      try {
+        await AuthService.registerUser({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'password123',
+          address: '123 Main St',
+          coordinates: [25.774, -80.19],
+        });
+      } catch (error) {
+        expect((error as any).message).to.equal(
+          'Either address or coordinates should be provided',
+        );
+      }
+
+      expect(findOneStub.calledOnce).to.be.true;
+    });
+
+    it('should throw an error if address is not provided and coordinates are invalid', async () => {
+      findOneStub.resolves(null);
+      sinon.stub(lib, 'getAddressFromCoordinates').resolves(null as any);
+
+      try {
+        await AuthService.registerUser({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'password123',
+          coordinates: [25.774, -80.19],
+        });
+      } catch (error) {
+        expect((error as any).message).to.equal('Address is required');
+      }
+
+      expect(findOneStub.calledOnce).to.be.true;
+      sinon.restore();
+    });
+
+    it('should throw an error if address is not provided and coordinates are not provided', async () => {
+      findOneStub.resolves(null);
+
+      try {
+        await AuthService.registerUser({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'password123',
+        });
+      } catch (error) {
+        expect((error as any).message).to.equal('Address is required');
+      }
+
+      expect(findOneStub.calledOnce).to.be.true;
     });
   });
 
-  describe('deleteUser', () => {
-    it('should delete a user if found', async () => {
-      const user = { _id: 'userId', name: 'John Doe' };
-      findByIdAndDeleteStub.resolves(user);
+  describe('loginUser', () => {
+    it('should login a user and return tokens', async () => {
+      const user = { _id: '1', name: 'John Doe', password: 'hashedPassword' };
+      findOneStub.resolves(user);
+      compareStub.resolves(true);
 
-      const result = await UserService.deleteUser({} as User, 'userId');
+      signStub
+        .onFirstCall()
+        .returns('token')
+        .onSecondCall()
+        .returns('refreshToken');
 
-      expect(result).to.deep.equal(user);
-      expect(findByIdAndDeleteStub.calledOnce).to.be.true;
+      const result = await AuthService.loginUser({
+        email: 'john@example.com',
+        password: 'password123',
+      });
+
+      expect(result).to.deep.equal({
+        token: 'token',
+        refreshToken: 'refreshToken',
+      });
+
+      expect(findOneStub.calledOnce).to.be.true;
+      expect(compareStub.calledOnce).to.be.true;
+      expect(signStub.calledTwice).to.be.true;
+    });
+
+    it('should throw an error if the credentials are invalid', async () => {
+      findOneStub.resolves(null);
+
+      try {
+        await AuthService.loginUser({
+          email: 'john@example.com',
+          password: 'password123',
+        });
+      } catch (error) {
+        expect((error as any).message).to.equal('Invalid credentials');
+      }
+
+      expect(findOneStub.calledOnce).to.be.true;
+    });
+  });
+
+  describe('updatePassword', () => {
+    it('should update the user password', async () => {
+      const user = { _id: '1', password: 'oldPassword' };
+      findOneStub.resolves(user);
+      hashStub.resolves('newHashedPassword');
+      updateOneStub.resolves();
+
+      const result = await AuthService.updatePassword('1', 'newPassword123');
+
+      expect(result).to.deep.equal({
+        message: 'Password updated successfully',
+      });
+
+      expect(findOneStub.calledOnce).to.be.true;
+      expect(hashStub.calledOnce).to.be.true;
+      expect(updateOneStub.calledOnce).to.be.true;
     });
 
     it('should throw an error if the user is not found', async () => {
-      findByIdAndDeleteStub.resolves(null);
+      findOneStub.resolves(null);
 
       try {
-        await UserService.deleteUser({} as User, 'userId');
+        await AuthService.updatePassword('1', 'newPassword123');
       } catch (error) {
         expect((error as any).message).to.equal('User not found');
       }
 
-      expect(findByIdAndDeleteStub.calledOnce).to.be.true;
+      expect(findOneStub.calledOnce).to.be.true;
     });
   });
 
-  describe('getUsers', () => {
-    it('should return a list of users', async () => {
-      const users = [{ _id: 'userId', name: 'John Doe' }];
+  describe('RefreshToken', () => {
+    it('should refresh the token and return new tokens', async () => {
+      const payload = { id: '1', name: 'John Doe' };
+      const user = { _id: '1', name: 'John Doe' };
+      verifyStub.resolves(payload);
+      findOneStub.resolves(user);
 
-      findStub.returns({
-        sort: sinon.stub().returnsThis(),
-        skip: sinon.stub().returnsThis(),
-        limit: sinon.stub().returnsThis(),
-        select: sinon.stub().resolves(users),
-      } as any);
+      signStub
+        .onFirstCall()
+        .returns('newToken')
+        .onSecondCall()
+        .returns('newRefreshToken');
 
-      const query: PagingDto = { page: 1, limit: 10 };
-      const result = await UserService.getUsers(query);
+      const result = await AuthService.RefreshToken('refreshToken');
 
-      expect(result).to.deep.equal(users);
-      expect(findStub.calledOnce).to.be.true;
+      expect(result).to.deep.equal({
+        token: 'newToken',
+        refreshToken: 'newRefreshToken',
+      });
+
+      expect(verifyStub.calledOnce).to.be.true;
+      expect(findOneStub.calledOnce).to.be.true;
+      expect(signStub.calledTwice).to.be.true;
+    });
+
+    it('should throw an error if the token is invalid', async () => {
+      verifyStub.throws(new Error('Invalid token'));
+
+      try {
+        await AuthService.RefreshToken('invalidToken');
+      } catch (error) {
+        expect((error as any).message).to.equal('Invalid token');
+      }
+
+      expect(verifyStub.calledOnce).to.be.true;
     });
   });
 });
