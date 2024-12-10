f3dc624da75a5422b8de11cd53c97c3d14da79c3
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 22:11:28 -0300

    Add environment configuration for MongoDB and JWT settings

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+export const env = {
+  MONGO_URI:
+    "mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin",
+  JWT_SECRET: "b2ZmaWNlLWJhY2tlbmQtc2VjcmV0LWtleQ==",
+  JWT_EXPIRES_IN: "1d",
+  JWT_REFRESH_SECRET: "b2ZmaWNlLWJhY2tlbmQtc2VjcmV0LXJlZnJlc2g=",
+  JWT_REFRESH_EXPIRES_IN: "7d",
+  PORT: 3000,
+  NODE_ENV: "development",
+};
