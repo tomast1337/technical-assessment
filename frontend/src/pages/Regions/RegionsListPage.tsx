9992a10f0fa558f58c815fecfdb5861d17ad53ac
tomast1337 <vycasnicolas@gmail.com>
2024-12-09 22:11:31 -0300

    Add RegionsListPage component to display a list of regions

---
+++ frontend/src/pages/Regions/RegionsListPage.tsx
@@ -0,0 +1,10 @@
+import { AppLayout } from "@components/Layout/AppLayout";
+import { RegionList } from "@components/Region/ListRegions";
+
+export const RegionsListPage = () => {
+  return (
+    <AppLayout>
+      <RegionList />
+    </AppLayout>
+  );
+};
